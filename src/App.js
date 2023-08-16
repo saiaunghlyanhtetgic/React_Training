cca7706cf81f7ff64da098797fc098498648feb7
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-16 12:51:24 +0630

    travel

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 // import Tenzies from './Tenzies';
 // import Meme from './Meme';
 // import ReactSite from './ReactSite';
-import SoloProject1 from './SoloProject1';
-// import SoloProject2 from './SoloProject2';
+// import SoloProject1 from './SoloProject1';
+import SoloProject2 from './SoloProject2';
 
 
 function App() {
   return (
     <>
-    <SoloProject1/>
+    <SoloProject2/>
     </>
   );
 }
