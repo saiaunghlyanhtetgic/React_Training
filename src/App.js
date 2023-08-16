3eb0da4824f3a1721e3c68c920ab9544c7d067ac
saiaunghlyanhtetgic <saiaunghlyanhtet@gicjp.com>
2023-08-16 12:46:55 +0630

    business card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 // import Tenzies from './Tenzies';
 // import Meme from './Meme';
-import ReactSite from './ReactSite';
-// import SoloProject1 from './SoloProject1';
+// import ReactSite from './ReactSite';
+import SoloProject1 from './SoloProject1';
 // import SoloProject2 from './SoloProject2';
 
 
 function App() {
   return (
     <>
-    <ReactSite/>
+    <SoloProject1/>
     </>
   );
 }
